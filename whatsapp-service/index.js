8cd9747ddbc381b51ca63aaef748948fb16fad88
Tasin Sohad <tasinthemindfisher@gmail.com>
2026-04-14 16:41:06 +0600

    Update Puppeteer userAgent to prevent linking errors

--- whatsapp-service/index.js
+++ whatsapp-service/index.js
@@ -39,7 +39,8 @@ const client = new Client({
       '--no-first-run',
       '--no-zygote'
     ],
-    headless: true
+    headless: true,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
   }
 });
 
